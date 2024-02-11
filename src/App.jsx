2a3b76297d4c175d8ebe4e83316dd3a4ecc02d09
pkthunder87 <pkthunder87@gmail.com>
2024-02-11 08:10:49 -0800

    Feat: Add initial code to NotificationItem

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import NotificationsTop from './components/NotificationsTop';
 
 function App() {
   return (
-    <div className="grid h-[80dvh] w-[52dvw] grid-rows-[12.5%_87.5%] rounded-xl bg-white px-8 py-4 text-2xl drop-shadow-sm">
+    <div className="grid h-[84dvh] w-[56dvw] grid-rows-[12.5%_87.5%] rounded-xl bg-white px-8 py-4 text-2xl drop-shadow-sm">
       <NotificationsTop />
       <NotificationsList />
     </div>
