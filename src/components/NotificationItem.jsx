f4bf919ac4d321aaae3d7f578a74c404a159ed21
pkthunder87 <pkthunder87@gmail.com>
2024-02-12 13:44:55 -0800

    Feat: Add profile pics and names

--- src/components/NotificationItem.jsx
+++ src/components/NotificationItem.jsx
@@ -1,15 +1,11 @@
-function NotificationItem() {
+function NotificationItem({ profilePic, profileName }) {
   return (
     <div className="grid h-[12.5%] w-full grid-cols-[10%_90%] grid-rows-2 items-center gap-x-4 rounded-lg bg-very-light-grayish-blue p-2">
       <div className="row-span-2 ml-2">
-        <img
-          className="h-12 w-12"
-          src="./avatar-mark-webber.webp"
-          alt="Profile image"
-        />
+        <img className="h-12 w-12" src={`${profilePic}`} alt="Profile image" />
       </div>
       <div className=" -ml-1 mt-2 self-start text-wrap text-base">
-        <span className="font-extrabold">Mark Webber</span>
+        <span className="font-extrabold">{`${profileName}`}</span>
         <span className="ml-2 text-dark-grayish-blue">
           reacted to your recent post
         </span>
--- src/components/NotificationsList.jsx
+++ src/components/NotificationsList.jsx
@@ -3,14 +3,34 @@ import NotificationItem from './NotificationItem';
 function NotificationsList() {
   return (
     <div className="flex flex-col gap-2 bg-white ">
-      <NotificationItem />
-      <NotificationItem />
-      <NotificationItem />
-      <NotificationItem />
-      <NotificationItem />
-      <NotificationItem />
-      <NotificationItem />
-      <NotificationItem />
+      <NotificationItem
+        profilePic="./avatar-mark-webber.webp"
+        profileName="Mark Webber"
+      />
+      <NotificationItem
+        profilePic="./avatar-angela-gray.webp"
+        profileName="Angela Gray"
+      />
+      <NotificationItem
+        profilePic="./avatar-jacob-thompson.webp"
+        profileName="Jacob Thompson"
+      />
+      <NotificationItem
+        profilePic="./avatar-rizky-hasanuddin.webp"
+        profileName="Rizky Hasanuddin"
+      />
+      <NotificationItem
+        profilePic="./avatar-kimberly-smith.webp"
+        profileName="Kimberly Smith"
+      />
+      <NotificationItem
+        profilePic="./avatar-nathan-peterson.webp"
+        profileName="Nathan Peterson"
+      />
+      <NotificationItem
+        profilePic="./avatar-anna-kim.webp"
+        profileName="Anna Kim"
+      />
     </div>
   );
 }
