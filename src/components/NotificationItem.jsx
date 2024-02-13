c3d714a9d4459cea363183f2081ac6dea1e837ad
pkthunder87 <pkthunder87@gmail.com>
2024-02-13 09:45:59 -0800

    Feat: Add custom titles, dates, and messages

--- src/components/NotificationItem.jsx
+++ src/components/NotificationItem.jsx
@@ -1,4 +1,12 @@
-function NotificationItem({ profilePic, profileName }) {
+function NotificationItem({
+  profilePic,
+  profileName,
+  reaction,
+  timestamp,
+  headline,
+  marginTop = '-mt-4',
+  link = '',
+}) {
   return (
     <div className="grid h-[12.5%] w-full grid-cols-[10%_90%] grid-rows-2 items-center gap-x-4 rounded-lg bg-very-light-grayish-blue p-2">
       <div className="row-span-2 ml-2">
@@ -6,17 +14,17 @@ function NotificationItem({ profilePic, profileName }) {
       </div>
       <div className=" -ml-1 mt-2 self-start text-wrap text-base">
         <span className="font-extrabold">{`${profileName}`}</span>
-        <span className="ml-2 text-dark-grayish-blue">
-          reacted to your recent post
-        </span>
-        <span className="ml-2 font-extrabold text-dark-grayish-blue">
-          My first tournament today!
+        <span className="ml-2 text-dark-grayish-blue">{`${reaction}`}</span>
+        <span className={`ml-2 font-extrabold text-dark-grayish-blue ${link}`}>
+          {`${headline}`}
         </span>
         <span className="">
           <div className=" inline-block h-3 w-3 scale-50 rounded-full bg-red"></div>
         </span>
       </div>
-      <div className="-ml-1 -mt-1 text-base text-dark-grayish-blue">1m ago</div>
+      <div
+        className={`-ml-1  ${marginTop} text-base text-dark-grayish-blue`}
+      >{`${timestamp}`}</div>
     </div>
   );
 }
--- src/components/NotificationsList.jsx
+++ src/components/NotificationsList.jsx
@@ -1,35 +1,62 @@
 import NotificationItem from './NotificationItem';
 
 function NotificationsList() {
+  const activeMessage =
+    " Hello, thanks for setting up the Chess Club. I've been a member for a few weeks now and I'm already having lots of fun and improving my game.";
+
   return (
     <div className="flex flex-col gap-2 bg-white ">
       <NotificationItem
         profilePic="./avatar-mark-webber.webp"
         profileName="Mark Webber"
+        reaction="reacted to your recent post"
+        timestamp="1m ago"
+        headline="My first tournament today!"
       />
       <NotificationItem
         profilePic="./avatar-angela-gray.webp"
         profileName="Angela Gray"
+        reaction="followed you"
+        timestamp="5m ago"
+        headline=""
       />
       <NotificationItem
         profilePic="./avatar-jacob-thompson.webp"
         profileName="Jacob Thompson"
+        reaction="has joined your group"
+        timestamp="1 day ago"
+        headline="Chess Club"
+        link="cursor-pointer text-blue"
       />
       <NotificationItem
         profilePic="./avatar-rizky-hasanuddin.webp"
         profileName="Rizky Hasanuddin"
+        reaction="sent you a private message"
+        timestamp="5 days ago"
+        headline=""
       />
       <NotificationItem
         profilePic="./avatar-kimberly-smith.webp"
         profileName="Kimberly Smith"
+        reaction="commented on your picture"
+        timestamp="1 week ago"
+        headline=""
       />
       <NotificationItem
         profilePic="./avatar-nathan-peterson.webp"
         profileName="Nathan Peterson"
+        reaction="reacted to your recent post"
+        timestamp="2 weeks ago"
+        headline="5 end-game strategies to increase your win rate"
+        marginTop="mt-6"
       />
       <NotificationItem
         profilePic="./avatar-anna-kim.webp"
         profileName="Anna Kim"
+        reaction="left the group"
+        timestamp="2 weeks ago"
+        headline="Chess Club"
+        link="cursor-pointer text-blue"
       />
     </div>
   );
