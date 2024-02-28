0705979a6a9e5cbd6bd5d59b2736df72748f2243
pkthunder87 <pkthunder87@gmail.com>
2024-02-28 14:02:20 -0800

    Feat: Modify sizes for desktop design in App

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import NotificationsTop from './components/NotificationsTop';
 
 function App() {
   return (
-    <div className="grid h-[100dvh] w-full overflow-scroll rounded-xl bg-white px-4 py-4 text-2xl shadow-[0_0_20px_25px_rgba(138,154,183,.1)] sm:h-[79.2rem] sm:w-[56dvw]  sm:px-8">
+    <div className="grid h-[100dvh] w-full overflow-scroll rounded-[16px] bg-white px-4 py-4 text-2xl shadow-[0_0_20px_25px_rgba(138,154,183,.08)] sm:mb-8 sm:h-[52.5rem] sm:w-[45.5rem] sm:px-8">
       <NotificationsTop />
       <NotificationsList />
     </div>
